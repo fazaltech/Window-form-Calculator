b7eb672ec9ff6727511aaae6784e48b460ed9f0c
fazaltech <fazal.abbas4@gmail.com>
2020-03-13 11:44:45 +0500

    Update Form1.Designer.cs

--- mywindowapp/mywindowapp/Form1.Designer.cs
+++ mywindowapp/mywindowapp/Form1.Designer.cs
@@ -112,7 +112,23 @@
             // 
             // button6
             // 
-           
+            this.button6.Location = new System.Drawing.Point(25, 100);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(106, 100);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
