67fe64ead6049ba3453d1539884b01df29d9b83b
fazaltech <fazal.abbas4@gmail.com>
2020-03-13 11:42:57 +0500

    Update Form1.Designer.cs

--- mywindowapp/mywindowapp/Form1.Designer.cs
+++ mywindowapp/mywindowapp/Form1.Designer.cs
@@ -22,10 +22,7 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
+        
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
