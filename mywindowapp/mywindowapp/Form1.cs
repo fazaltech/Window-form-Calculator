5f5afacf226cc056ed2e1fd6a45e09d61b6c4124
fazaltech <fazal.abbas4@gmail.com>
2020-03-13 11:35:25 +0500

    Update Form1.cs

--- mywindowapp/mywindowapp/Form1.cs
+++ mywindowapp/mywindowapp/Form1.cs
@@ -16,7 +16,7 @@ namespace mywindowapp
         static string IsOperand = "";
         static string myval = "";
 
-        public Form()
+        public Form1()
         {
             InitializeComponent();
         }
